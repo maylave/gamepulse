bc27285011bb2bff9b4250800d027231995a8231
Artem <prrpappspl@gmail.com>
2025-12-15 16:38:16 +0300

    fix home hero

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -60,7 +60,7 @@ import { api } from '@/services/api'
 
 import Header from '@/components/Header.vue'
 import Footer from '@/components/footer.vue'
-import Hero from '@/components/Hero.vue'
+import Hero from '@/components/hero.vue'
 import GenreCategories from '@/components/GenreCategories.vue'
 import GamesCarousel from '@/components/GamesCarousel.vue'
 
